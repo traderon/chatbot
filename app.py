ef4d727dd62f1cdfaa0dca8b9f1a1878e3337a7b
traderon <chelizerb@gmail.com>
2026-07-22 12:02:00 -0500

    Agregar runtime de Python..

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ CORS(app)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
+    print("Entró a index")
     global preguntas
     global contexto
     global pregunta
